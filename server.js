1a4578ecc8060387eef1dc74bd8955b91470dced
unknown <Puinun>
2018-09-17 09:25:14 +0700

    put code server

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+ var pgp = require('mysql')
+ var connection = mysql.createConnection({
+    host     : 'https://www.db4free.net',
+    user     : 's140390',
+    password : 'abc123**',
+    database : 'db140390'
+ });
