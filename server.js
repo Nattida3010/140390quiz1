d76fdb2ac4701e8c87d6f83cdc4d6d2adeb887bc
unknown <Puinun>
2018-09-17 10:00:42 +0700

    put code server

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
- var mysql = require('mysql')
- var connection = mysql.createConnection({
-    host     : 'https://www.db4free.net',
-    user     : 's140390',
-    password : 'abc123**',
-    database : 'db140390'
- });
- app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-    res.render('pages/index');
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
 });
+connection.connect();
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/home', function (req, res){
+    connection.query('SELECT * FROM students', function (error, results, fields) {
+        console.log('DATA'+results);
+        res.render('pages/home',{home : results})
+      });
+});
+
 console.log('App is running at http://localhost:8081');
+
 app.listen(8081);
